14ecedc07358765e4cc2c3e9be448458a0cb5e5c
tombs007 <thetruthisntoutthere@gmail.com>
2020-10-27 22:46:49 -0400

    Changed name

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -131,7 +131,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "KP3S"
+#define CUSTOM_MACHINE_NAME "Crypt1cT3ch"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
